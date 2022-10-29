9eabc6ecab66184811c310fbbc6268e122c9544a
James Xalis <jamesxalis@icloud.com>
2022-10-28 17:08:27 -0700

    added post direction

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -8,5 +8,15 @@ router.get('/notes', (req,res) => {
     })
 })
 
+router.post('/notes', (req,res) => {
+    req.body.id = Math.floor(Math.random()*4000);
+    fs.readFile("db/db.json", (err, data) => {
+        const db = JSON.parse(data);
+        db.push(req.body);
+        fs.writeFile("db/db.json", JSON.stringify(db, null, 2), err => err ? console.log(err) : console.log("Successfully added note entry"));
+    })
+    res.json(req.body);
+})
+
 
 module.exports = router;
